02eeb48df40c97e461d702fd30071cc674267d29
Otis <altispec@gmail.com>
2015-10-17 12:55:34 +0800

    Modify routes to CV

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
+  get 'cv', to: 'pages#cv'
   get 'pages/cv'
 
   root 'articles#index'
 
   resources :articles, only: [:index, :show]
 
-  namespace :backend do
-    root 'articles#index'
-    resources :articles do
-      post :publish, on: :member
-      post :unpublish, on: :member
-    end
-  end
+  # namespace :backend do
+  #   root 'articles#index'
+  #   resources :articles do
+  #     post :publish, on: :member
+  #     post :unpublish, on: :member
+  #   end
+  # end
 end
